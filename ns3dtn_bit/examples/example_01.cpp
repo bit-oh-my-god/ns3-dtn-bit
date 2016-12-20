404aa947ecf598fb4cdfd442eebc914808a72008
DeathPoem <wangzhihongji@sina.cn>
2016-12-20 04:54:15 -0800

    2016-12-20 local commit

--- ns3dtn_bit/examples/example_01.cpp
+++ ns3dtn_bit/examples/example_01.cpp
@@ -1,2 +1,48 @@
 #include "ns3/core-module.h"
-#include "../model/dtn_package.h"
+#include "ns3/ns3dtn_bit"
+
+namespace ns3 {
+    namespace ns3dtn_bit {
+        
+        class Example_01 {
+        public:
+            Example_01 ();
+            virtual ~Example_01 ();
+            bool Configure();
+            void Run();
+        
+        private:
+            
+        };
+
+        Example_01::Example_01() {
+
+        }
+
+        bool Example_01::Configure() {
+            return true;
+
+        }
+
+        void Example_01::Run() {
+
+        }
+        
+    } /* ns3dtn_bit */ 
+    
+} /* ns3  */ 
+
+int main() {
+    // support c++11
+#if (__cplusplus==201103L)
+    auto test = ns3::ns3dtn_bit::Example_01();
+#else 
+    ns3::ns3dtn_bit::Example_01 test();
+#endif
+
+    if (! test.Configure()) {
+        NS_FATAL_ERROR("configure fail");
+    }
+
+    test.Run();
+}
---
+++ ns3dtn_bit/helper/ns3dtn_bit_helper.h
@@ -0,0 +1,4 @@
+#ifndef NS3DTN_BIT_HELPER_H
+#define NS3DTN_BIT_HELPER_H 
+
+#endif /* ifndef NS3DTN_BIT_HELPER_H */
