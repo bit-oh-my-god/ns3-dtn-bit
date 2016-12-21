2a514284bf673d6963242c410a8e26319da32014
DeathPoem <wangzhihongji@sina.cn>
2016-12-20 17:33:20 -0800

    2016-12-20 local commit

--- extract.sh
+++ extract.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-NS3TAR="~/Downloads/ns-allinone-3.25.tar.bz2"
+NS3TAR="/home/dtn-012345/Downloads/ns-allinone-3.25.tar.bz2"
 # create environment
 tar -xjvf $NS3TAR &> /dev/null
 cp -rf ./ns3dtn_bit ./ns-allinone-3.25/ns-3.25/src/
