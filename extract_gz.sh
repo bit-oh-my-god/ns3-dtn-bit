e50ca23ad8a7b83e1091ff8e14fa76c9caf8b275
DeathPoem <wangzhihongji@sina.cn>
2017-04-08 20:24:32 -0700

    2017-04-08 local commit

--- extract_gz.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-NS3TAR="/home/dtn-012345/Downloads/ns-allinone-3.26.tar.bz2"
-# create environment
-tar -xjvf $NS3TAR &> /dev/null
-cp -rf ./ns3dtn-bit ./ns-allinone-3.26/ns-3.26/src/
