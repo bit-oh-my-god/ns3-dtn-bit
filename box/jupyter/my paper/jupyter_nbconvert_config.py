2c73a1ded4439693a679c656ee66359541bc16b1
DeathPoem <wangzhihongji@sina.cn>
2017-05-11 17:37:07 +0800

    2017-05-11 local commit

--- box/jupyter/my paper/jupyter_nbconvert_config.py
+++
@@ -1,7 +0,0 @@
-c = get_config()
-
-#Export all the notebooks in the current directory to the sphinx_howto format.
-c.NbConvertApp.notebooks = ['papper_scratch.ipynb']
-c.NbConvertApp.export_format = 'pdf'
-
-c.Exporter.template_file = 'citations'
